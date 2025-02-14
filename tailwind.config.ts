b6a88eb5f706be58fd3b3c5c3920352267a8117c
cesar guajardo <cguajard@ucsd.edu>
2025-02-13 23:14:36 -0800

    hopefully fixed font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,4 @@
-import type { Config } from "tailwindcss";
 const withMT = require("@material-tailwind/react/utils/withMT");
-/** @type {import('tailwindcss').Config} */
-
-// export default {
-//   content: [
-//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//           geo: ['Geo', 'sans-serif'],
-//           orbitron: ['Orbitron','sans-serif']
-//       },
-//       colors: {
-//         background: "var(--background)",
-//         foreground: "var(--foreground)",
-//       },
-//     },
-//     keyframes: {
-//       scroll: {
-//         '0%': { transform: 'translateX(0)' },
-//         '100%': { transform: 'translateX(-100%)' },
-//       },
-//     },
-//     animation: {
-//       scroll: 'scroll 60s linear infinite',
-//     },
-//   },
-//   plugins: [require('@tailwindcss/forms')],
-// } satisfies Config;
-
-
 
 module.exports = withMT({
   content: [
@@ -43,48 +9,14 @@ module.exports = withMT({
   theme: {
     extend: {
       fontFamily: {
-          geo: ['Geo', 'sans-serif'],
-          orbitron: ['Orbitron','sans-serif']
+        geo: ["var(--font-geo)", "sans-serif"], // ✅ Use CSS variable
+        orbitron: ["Orbitron", "sans-serif"],
       },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
-    keyframes: {
-      scroll: {
-        '0%': { transform: 'translateX(0)' },
-        '100%': { transform: 'translateX(-100%)' },
-      },
-    },
-    animation: {
-      scroll: 'scroll 60s linear infinite',
-    },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require("@tailwindcss/forms")],
 });
-
-
-
-// theme: {
-//   extend: {
-//     fontFamily: {
-//       geo: ['Geo', 'sans-serif'],
-//       orbitron: ['Orbitron','sans-serif']
-//     },
-//     keyframes: {
-//       scroll: {
-//         '0%': { transform: 'translateX(0)' },
-//         '100%': { transform: 'translateX(-100%)' },
-//       },
-//     },
-//     animation: {
-//       scroll: 'scroll 60s linear infinite',
-//     },
-//     colors: {
-//       customGray: '#BEBEBE', // Add the custom color
-//     },
-// },
-//   plugins: [
-//     require('@tailwindcss/forms'),
-//   ],
