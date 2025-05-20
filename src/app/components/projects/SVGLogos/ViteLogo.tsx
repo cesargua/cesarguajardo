f9af2c46655ac7d2087919761db9068bc89139ee
cesar guajardo <cguajard@ucsd.edu>
2025-05-19 20:23:14 -0700

    card fix

--- src/app/components/projects/SVGLogos/ViteLogo.tsx
+++ src/app/components/projects/SVGLogos/ViteLogo.tsx
@@ -3,7 +3,7 @@
 const ViteLogo: React.FC = () => {
     return (
       <img
-         src="/ViteJS.svg"
+         src="/ViteJs.svg"
         alt="Vitejs.js Logo"
         className="h-12 w-auto hover:opacity-80 transition-opacity duration-200"
         title="Vite"
